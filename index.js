74e58fd164eed27549908474ef26cd7dd8f35eb3
Mehadi Hasan <Mehadihasan.phy@gmail.com>
2022-05-28 11:04:26 +0600

    Verify admin method added

--- index.js
+++ index.js
@@ -42,6 +42,18 @@ async function run() {
         const orderCollection = client.db("toolkits").collection("order");
         const informationCollection = client.db("toolkits").collection("information");
 
+
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+          const requesterAccount = await userCollection.findOne({ email: requester });
+          if (requesterAccount.role === 'admin') {
+            next();
+          }
+          else {
+            res.status(403).send({ message: 'forbidden' });
+          }
+        }
+
         // GET
 
          app.get('/product', async (req, res) =>{
@@ -51,7 +63,7 @@ async function run() {
              res.send(products);
          });
 
-         app.get('/user', async (req, res) => {
+         app.get('/user', verifyJWT, async (req, res) => {
             const users = await usersCollection.find().toArray();
             res.send(users);
           });
@@ -115,15 +127,15 @@ async function run() {
 
         // PUT 
 
-          app.put('/user/admin/:email', async (req, res) => {
+          app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
             const filter = { email: email };
             const updateDoc = {
               $set: { role: 'admin' },
             };
             const result = await usersCollection.updateOne(filter, updateDoc);
-            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
-            res.send({result, token});
+            
+            res.send(result);
           }) 
 
          app.get('/product/:id', async (req, res) => {
